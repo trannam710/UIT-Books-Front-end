34e51a2a68050d0c02248fc67c9c4a066001d51e
TranNam <trandinhnam071021@gmail.com>
2023-01-17 15:46:11 +0700

    complete admin template

--- js/admin.js
+++ js/admin.js
@@ -46,10 +46,10 @@
 //   })
 // })()
 
-// DASHBOARD BAR CHART
+// DASHBOARD PIE CHART
 
 var ctx = document.getElementById("barChart");
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var xValues = ["Văn Học", "SGK", "Truyện Tranh", "Ngoại Ngữ", "CNTT"];
 var yValues = [55, 49, 44, 24, 15];
 var barColors = [
   "#b91d47",
@@ -71,29 +71,27 @@ new Chart(ctx, {
   options: {
     title: {
       display: true,
-      text: "World Wide Wine Production 2018"
+      text: "Tỉ lệ sách bán trong năm 2022"
     }
   }
 });
 
 // DASHBOARD LINE CHART
 var ctx = document.getElementById("lineChart");
-var xValues = [100,200,300,400,500,600,700,800,900,1000];
+var xValues = ['1','2','3','4','5','6','7','8','9','10', '11', '12'];
 
 new Chart("lineChart", {
 type: "line",
 data: {
     labels: xValues,
-    datasets: [{ 
-    data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-    borderColor: "red",
-    fill: false
-    }, { 
-    data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+    datasets: [
+    { 
+    data: [1600,1760,1806,1900,2000,2700,3034,4553,5306,6135,6902,7204],
     borderColor: "green",
     fill: false
-    }, { 
-    data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+    }, 
+    { 
+    data: [300,700,2000,4500,5300,4000,3000,3500,5000,5800,5500,6200],
     borderColor: "blue",
     fill: false
     }]
